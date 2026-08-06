e65bb6d1d32e5aba1f9bb3cd40953ff37cae1451
alexsigei <alex.sigei@student.moringaschool.com>
2026-08-06 23:00:47 +0300

    Article list done and bug fix in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import Header from './components/Header'
 import About from './components/About'
 import blog from './blog'
+import ArticleList from './components/ArticleList'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Header name={blog.name} />
@@ -18,11 +14,7 @@ function App() {
         image={blog.image}
         about={blog.about}
       />
-      <Article 
-        title={post.title}
-        date={post.date}
-        preview={post.preview}
-      />
+      <ArticleList posts={blog.posts}/>
     </>
   )
 }
