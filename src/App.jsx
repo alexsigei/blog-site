bc80bc30de01cd6c9e6d0e993ce0501ebe61392b
alexsigei <alex.sigei@student.moringaschool.com>
2026-08-06 20:11:30 +0300

    About component done

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Header from './components/Header'
+import About from './components/About'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Header name={blog.name} />
+
+      <About
+        image={blog.image}
+        about={blog.about}
+      />
     </>
   )
 }
